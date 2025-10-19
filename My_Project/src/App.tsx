97ee42efba80c6736263f51befacb0c852d51911
caokaixiang <alexcao911@outlook.com>
2025-10-20 00:31:41 +0800

    Handle 3D assets

--- My_Project/src/App.tsx
+++ My_Project/src/App.tsx
@@ -1,6 +1,4 @@
-import { useState } from 'react'
-import reactLogo from './assets/react.svg'
-import viteLogo from '/vite.svg'
+import { useState } from 'react';
 import './App.css';
 import { Route, BrowserRouter as Router, Routes } from 'react-router-dom';
 import Navbar from './components/Navbar.tsx';
@@ -10,12 +8,12 @@ const App = () => {
   return (
     <main className='bg-slate-300/20'>
       <Router>
-        <Navbar/>
+        <Navbar />
         <Routes>
-         <Route path="/" element={<Home/>} />
-         <Route path="/about" element={<About/>} />
-         <Route path="/projects" element={<Projects/>} />
-         <Route path="/contact" element={<Contact/>} />
+          <Route path="/" element={<Home />} />
+          <Route path="/about" element={<About />} />
+          <Route path="/projects" element={<Projects />} />
+          <Route path="/contact" element={<Contact />} />
         </Routes>
       </Router>
     </main>
--- My_Project/src/pages/Home.tsx
+++ My_Project/src/pages/Home.tsx
@@ -1,8 +1,20 @@
-import React from 'react'
+import { Suspense } from 'react';
+import Loader from '../components/Loader';
+import { Canvas } from '@react-three/fiber'
 
 const Home = () => {
   return (
-    <div>Home</div>
+    <section className='w-full h-screen relative'>Home
+        <div className='absolute top-28 left-0 right-0 z-10 flex items-center justify-center'>
+        </div>
+        <Canvas className='w-full h-screen bg-transparent' camera={{near: 0.1, far: 1000}} >
+            <Suspense fallback={<Loader />}>
+                <ambientLight />
+                <directionalLight />
+                <hemisphereLight />
+            </Suspense>
+        </Canvas>
+    </section>
   )
 }
 
