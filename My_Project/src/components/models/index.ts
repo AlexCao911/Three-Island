8a50a73cac26740ca15d943e7376d72d619e5887
caokaixiang <alexcao911@outlook.com>
2025-10-20 21:32:58 +0800

    feat:island models

---
+++ My_Project/src/components/models/index.ts
@@ -0,0 +1,9 @@
+// 3D Models Export
+export { Bird } from './Bird'
+export { Fox } from './Fox'
+export { Island } from './Island'
+export { Plane } from './Plane'
+export { Sky } from './Sky'
+export { IcelandIsland } from './Iceland_island'
+export { IslandWithTowerV1 } from './Spaceship'
+export { IslandWithTowerV2 } from './Island_with_tower'
